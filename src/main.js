3b536c1da4ed951b5c719aa769c20402106d296d
lichtle <leticia.negreiros@outlook.com>
2023-09-27 11:51:08 -0300

    chore: adicionada a seção de perguntas frequentes

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const buttons = this.querySelectorAll("[data-tab-button]"); // Pegando todos os botões que possuem a propriedade data-tab-button
+  const buttons = document.querySelectorAll("[data-tab-button]"); // Pegando todos os botões que possuem a propriedade data-tab-button
+  const questions = document.querySelectorAll("[data-faq-question]"); // Pegando todas as divs de pergunta que possuem a propriedade data-tab-button
 
   for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", function (btn) {
@@ -14,6 +15,10 @@ document.addEventListener("DOMContentLoaded", function () {
 
       btn.target.classList.add("shows__tabs__button--is-active");
     });
+  }
+
+  for (let i = 0; i < questions.length; i++) {
+    questions[i].addEventListener("click", abreOuFechaResposta); // Ao clicar na pergunta, a função de abrir ou fechar a resposta é chamada. A função tem como retorno a própria div de pergunta, mas precisamos encontrar o elemento pai (li), pois é nele que o modificador --is-open será aplicado
   }
 });
 
@@ -31,4 +36,11 @@ function hideAllTabs() {
   for (let i = 0; i < tabs.length; i++) {
     tabs[i].classList.remove("shows__list--is-active");
   }
+}
+
+function abreOuFechaResposta(elemento) {
+  const classe = "faq__questions__item--is-open";
+  const elementoPai = elemento.target.parentNode; // Ao clicar na pergunta, a função de abrir ou fechar a resposta é chamada. A função tem como retorno a própria div de pergunta, mas precisamos encontrar o elemento pai (li), pois é nele que o modificador --is-open será aplicado
+
+  elementoPai.classList.toggle(classe);
 }
