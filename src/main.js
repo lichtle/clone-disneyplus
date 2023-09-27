141fc1e7d8ec33dee811a4853504f705a4fb905d
lichtle <leticia.negreiros@outlook.com>
2023-09-27 14:15:01 -0300

    chore: adicionada o header da página

--- src/main.js
+++ src/main.js
@@ -1,6 +1,33 @@
 document.addEventListener("DOMContentLoaded", function () {
+  // SCROLL DO HEADER
+
+  const heroSection = document.querySelector(".hero"); // Armazenando o hero à uma variável para posteriormente pegar sua altura
+  const alturaHero = heroSection.clientHeight; // Pegando a altura do hero
+
+  window.addEventListener("scroll", function () {
+    // Identificando o scroll do usuário
+    const posicaoAtual = window.scrollY; // Identificando a posição da barra de rolagem
+
+    if (posicaoAtual < alturaHero) {
+      ocultaHeader();
+    } else {
+      exibeHeader();
+    }
+  });
+
+  function ocultaHeader() {
+    const header = document.querySelector("header");
+    header.classList.add("header--is-hidden");
+  }
+
+  function exibeHeader() {
+    const header = document.querySelector("header");
+    header.classList.remove("header--is-hidden");
+  }
+
+  // SEÇÃO DE ATRAÇÕES (ABAS)
+
   const buttons = document.querySelectorAll("[data-tab-button]"); // Pegando todos os botões que possuem a propriedade data-tab-button
-  const questions = document.querySelectorAll("[data-faq-question]"); // Pegando todas as divs de pergunta que possuem a propriedade data-tab-button
 
   for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", function (btn) {
@@ -17,26 +44,30 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 
+  function removeActive() {
+    const buttons = document.querySelectorAll("[data-tab-button]");
+
+    for (let i = 0; i < buttons.length; i++) {
+      buttons[i].classList.remove("shows__tabs__button--is-active");
+    }
+  }
+
+  function hideAllTabs() {
+    const tabs = document.querySelectorAll("[data-tab-id]");
+
+    for (let i = 0; i < tabs.length; i++) {
+      tabs[i].classList.remove("shows__list--is-active");
+    }
+  }
+
+  // SEÇÃO DE PERGUNTAS FREQUENTES (ACCORDION)
+
+  const questions = document.querySelectorAll("[data-faq-question]"); // Pegando todas as divs de pergunta que possuem a propriedade data-tab-button
+
   for (let i = 0; i < questions.length; i++) {
     questions[i].addEventListener("click", abreOuFechaResposta); // Ao clicar na pergunta, a função de abrir ou fechar a resposta é chamada. A função tem como retorno a própria div de pergunta, mas precisamos encontrar o elemento pai (li), pois é nele que o modificador --is-open será aplicado
   }
 });
-
-function removeActive() {
-  const buttons = document.querySelectorAll("[data-tab-button]");
-
-  for (let i = 0; i < buttons.length; i++) {
-    buttons[i].classList.remove("shows__tabs__button--is-active");
-  }
-}
-
-function hideAllTabs() {
-  const tabs = document.querySelectorAll("[data-tab-id]");
-
-  for (let i = 0; i < tabs.length; i++) {
-    tabs[i].classList.remove("shows__list--is-active");
-  }
-}
 
 function abreOuFechaResposta(elemento) {
   const classe = "faq__questions__item--is-open";
